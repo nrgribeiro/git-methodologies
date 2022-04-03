70ef6ab1bb469dafc104d9525c35fe6357913112
Nuno Ribeiro <nribeiro@gmail.com>
2022-04-03 17:16:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "react-bootstrap";
+import "./App.css";
+
+function App() {
+  return <ThemeProvider></ThemeProvider>;
+}
+
+export default App;
