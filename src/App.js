4742d95b29ed4bb82da3955c7031012e6964f724
gonzoespinho10 <fidalgogonzo@gmail.com>
2022-04-05 12:28:04 +0100

    feat: React Router & Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemeProvider } from "react-bootstrap";
 import "./App.css";
+import React from "react";
+import {Router} from "react-router-dom";
 
 function App() {
-  return <ThemeProvider></ThemeProvider>;
+  return(
+  <ThemeProvider>
+  <Router />
+  </ThemeProvider>
+)
 }
 
 export default App;
