e12a26b2bdb5b2fc4a9eebd992852dc8b54b8fb9
Beatriz Bastos <beatrizbastos@aluno-4059.wireless.ua.pt>
2022-04-05 12:38:46 +0100

    galeria

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemeProvider } from "react-bootstrap";
+import Gallery from "./pages/Gallery";
 import "./App.css";
 
 function App() {
-  return <ThemeProvider></ThemeProvider>;
+  return <ThemeProvider>
+    <Gallery />
+  </ThemeProvider>;
 }
 
 export default App;
